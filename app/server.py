25e1ce87bfe751f5c266bfbc0e123b78806006a0
alien35 <aleon6@u.rochester.edu>
2019-07-01 22:05:35 -0400

    [WIP] try original

--- app/server.py
+++ app/server.py
@@ -9,11 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/g3x68odj64mw2my/photos.pkl?raw=1'
-export_file_name = 'photos.pkl'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = 'export.pkl'
 
-classes = ['fake', 'real']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
